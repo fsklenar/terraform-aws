caf328ece788ec045e300303193c35110f28641c
Frantisek Sklenar <frantisek.sklenar@gmail.com>
2025-07-24 09:58:11 +0200

    terraform

--- aws-ec2-rds/env/dev/ec2_app.tf
+++ aws-ec2-rds/env/dev/ec2_app.tf
@@ -7,7 +7,6 @@ module "ec2-app" {
   ec2-ami           = var.ami-id
   subnet_id         = module.vpc_main.public_subnets[0]
   security_group_id = module.ec2_app_sg.security_group_id
-
   key_pair_name = module.key-pair-ec2.key_pair_name
   root_size     = var.root_size
 }
